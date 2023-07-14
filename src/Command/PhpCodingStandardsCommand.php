6ff29a5ee8476177457776db33cdc09ea2d29535
Mike Street <mike@liquidlight.co.uk>
2023-07-14 15:07:48 +0100

    build: Move packages out of subfolder

---
+++ src/Command/PhpCodingStandardsCommand.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace LiquidLight\PhpCodingStandards\Command;
+
+use App\Command\Base;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+use Symfony\Component\Console\Command\Command;
+
+use Symfony\Component\Console\Input\InputOption;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class PhpCodingStandardsCommand extends Base
+{
+	protected static $defaultName = 'php:coding-standards';
+
+	protected function configure(): void
+	{
+		parent::configure();
+
+		$this
+			// the short description shown while running "php bin/console list"
+			->setDescription('PHP Coding Standards')
+			->addOption(
+				'clean',
+				'',
+				InputOption::VALUE_NONE,
+				'Delete the cache file and do a clean scan',
+			)
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		parent::execute($input, $output);
+		$extPath = dirname(__DIR__) . '/..';
+
+		if ($this->input->getOption('clean')) {
+			$this->io->text('Deleting cache file');
+			unlink($this->path . '/.cache/.phpcscache_' . md5($GLOBALS['_SERVER']['PWD']));
+		}
+
+		$fileCheck = $this->hasFiles('php');
+		if (!$fileCheck) {
+			return Command::SUCCESS;
+		}
+
+		$command = [
+			$this->path . '/vendor/bin/php-cs-fixer',
+			'fix',
+			'--config=' . $extPath . '/resources/config/PHPCodingStandards.php',
+		];
+
+		if ($this->input->getOption('whisper')) {
+			$command[] = '--quiet';
+		}
+
+		if ($output->isVerbose() || $this->input->getOption('fix') || !Process::isTtySupported()) {
+			$command[] = '--verbose';
+		}
+
+		if ($output->isVeryVerbose()) {
+			$command[] = '--diff';
+		}
+
+		if ($this->input->getOption('fix') === false) {
+			$command[] = '--dry-run';
+		} else {
+			$this->io->text('Attempting to fix...');
+		}
+
+		$process = $this->cmd($command);
+		$this->io->newLine();
+		$this->outputResult($process);
+
+		return $process->isSuccessful() ? Command::SUCCESS : Command::FAILURE;
+	}
+}
